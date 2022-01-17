c1050e2f4a181e32e86a8b7a8e3d3871ca91be7f
Luk4x <luk4xm4ci3l@gmail.com>
2022-01-17 17:33:37 -0300

    add button 'voltar ao topo'

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let form = document.getElementById('contact-form')
 
+let footer = document.getElementById('doc-footer')
+toTopButton.addEventListener('click', backTop)
+
 function contactButton() {
     form.className = 'visible-form'
+}
+
+function backTop() {
+    scrollTo({top: 0, left: 0, behavior: 'smooth'})
 }

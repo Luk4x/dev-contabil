2551e950a053355a473350bd370892a1b89606f2
Luk4x <luk4xm4ci3l@gmail.com>
2022-02-09 11:25:14 -0300

    added responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let form = document.getElementById('contact-form')
-
 let footer = document.getElementById('doc-footer')
+let btnMobile = document.querySelector('#mobile-menu-btn')
+let nav2 = document.querySelector('.nav2')
+
 toTopButton.addEventListener('click', backTop)
+btnMobile.addEventListener('click', toggleMMneu)
 
 function contactButton() {
     if (form.className == 'invisible-form') {
@@ -13,4 +16,8 @@ function contactButton() {
 
 function backTop() {
     scrollTo({top: 0, left: 0, behavior: 'smooth'})
+}
+
+function toggleMMneu() {
+    nav2.classList.toggle('active')
 }
